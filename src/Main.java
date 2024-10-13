44dce3de87e1676f88c11edb71585ce408081957
LuanPc <luanlima747@gmail.com>
2024-10-13 18:11:20 -0300

    "Corrigido o fluxo de entrada de dados para evitar erros de captura de informações do proprietário." "Melhorada a exibição dos dados coletados para cada proprietário no método main."

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import propertytaxcalculator.Property;
+import propertytaxcalculator.PropertyOwner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        List<PropertyOwner> propertyOwners = new ArrayList<>();
+
+        System.out.print("Quantos proprietários: ");
+        int quantityOwner = sc.nextInt();
+
+        for (int i = 0; i < quantityOwner; i++) {
+            System.out.print("Nome proprietário: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Idade: ");
+            int age = sc.nextInt();
+
+            PropertyOwner propertyOwner = new PropertyOwner(name, age);
+
+            System.out.print("\nQuantas propriedades " + name + " Possui? ");
+            int quantityproperties = sc.nextInt();
+
+            for (int j = 0; j < quantityproperties; j++) {
+                System.out.print("Tipo: (Residencial / Comercial): ");
+                sc.nextLine();
+                String type = sc.nextLine();
+
+                System.out.print("Valor : ");
+                double value = sc.nextDouble();
+
+                System.out.print("Anos: ");
+                int yearsOld = sc.nextInt();
+
+
+                Property prop = new Property(type,value, yearsOld);
+                propertyOwner.getProperties().add(prop);
+            }
+
+            propertyOwners.add(propertyOwner);
+        }
+        for (PropertyOwner propertyOwner : propertyOwners) {
+            propertyOwner.displaySummary();
+        }
     }
 }
