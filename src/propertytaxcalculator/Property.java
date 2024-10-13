4a298c6dfcb80b0ece07077f83cb2925327390cd
LuanPc <luanlima747@gmail.com>
2024-10-13 16:12:17 -0300

    Implementa construtores, getters e setters nas classes: - PropertyOwner: nome, idade e lista de propriedades - Property: tipo, valor e idade do imóvel

--- src/propertytaxcalculator/Property.java
+++ src/propertytaxcalculator/Property.java
@@ -1,4 +1,36 @@
 package propertytaxcalculator;
 
 public class Property {
+    private String type;
+    private double value;
+    private int yearsOld;
+
+    public Property(String type, double value, int yearsOld) {
+        this.type = type;;
+        this.value = value;
+        this.yearsOld = yearsOld;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
 }
--- src/propertytaxcalculator/PropertyOwner.java
+++ src/propertytaxcalculator/PropertyOwner.java
@@ -1,4 +1,41 @@
 package propertytaxcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyOwner {
+    private String name;
+    private int age;
+    private List<Property> properties;
+
+    public PropertyOwner(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.properties = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
+    }
 }
