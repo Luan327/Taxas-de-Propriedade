f677068c778de649a91ebbeb8f251c23ffb51cfb
Luan327 <166567148+Luan327@users.noreply.github.com>
2024-10-13 20:18:44 -0300

    Criado um metodo de entrada de Dados diretamento no metodo.

--- src/propertytaxcalculator/PropertyOwner.java
+++ src/propertytaxcalculator/PropertyOwner.java
@@ -2,11 +2,15 @@ package propertytaxcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PropertyOwner {
     private String name;
     private int age;
     private List<Property> properties;
+
+    public PropertyOwner() {
+    }
 
     public PropertyOwner(String name, int age) {
         this.name = name;
@@ -45,8 +49,8 @@ public class PropertyOwner {
         for (Property property : properties) {
             valorTaxas += property.grossTax();
         }
-        if (getAge() > 60 && properties.size() > 1) {
-            valorTaxas = valorTaxas * 0.80;
+        if (age > 60 && properties.size() > 1) {
+            valorTaxas *= 0.80;
         }
         return valorTaxas;
     }
@@ -65,15 +69,73 @@ public class PropertyOwner {
             System.out.printf("imposto: R$ %.2f, Manutenção: R$ %.2f%n ",
                     property.taxProperty(),
                     property.serviceCharge());
-
             System.out.printf("Total de taxas sem desconto: R$ %.2f%n ", property.grossTax());
             System.out.println();
         }
-        if (properties.size() > 1) {
+
+        if (properties.size() > 1 && age > 60) {
             System.out.println("Desconto 20%");
         } else {
             System.out.println("Sem desconto");
         }
-        System.out.printf("Total a pagar: %.2f ", discount());
+        System.out.printf("Total a pagar: %.2f%n ", discount());
+    }
+
+    public void entradaDados(List<PropertyOwner> propertyOwners) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos proprietários: ");
+        int quantityOwner = sc.nextInt();
+
+        String type = "";
+        int tipo = 0;
+
+        for (int i = 0; i < quantityOwner; i++) {
+            System.out.print("Nome proprietário: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Idade: ");
+            int age = sc.nextInt();
+
+            PropertyOwner propertyOwner = new PropertyOwner(name, age);
+
+            System.out.print("\nQuantas propriedades " + name + " Possui? ");
+            int quantityproperties = sc.nextInt();
+
+            for (int j = 0; j < quantityproperties; j++) {
+
+                do {
+                    System.out.println("Selecione o tipo do Imóvel:");
+                    System.out.println("1 - Residencial");
+                    System.out.println("2 - Comercial");
+                    System.out.print("Entre com o tipo : ");
+                    tipo = sc.nextInt();
+                    if (tipo == 1) {
+                        type = "Residencial";
+                    } else if (tipo == 2) {
+                        type = "Comercial";
+                    } else {
+                        System.out.println("Tipo incorreto, selecione ( 1 ou 2 )");
+                    }
+                } while (tipo != 1 && tipo != 2);
+
+                System.out.print("Valor: ");
+                double value = sc.nextDouble();
+
+                System.out.print("Anos: ");
+                int yearsOld = sc.nextInt();
+                System.out.println();
+
+                Property prop = new Property(type, value, yearsOld);
+                propertyOwner.getProperties().add(prop);
+
+            }
+            propertyOwners.add(propertyOwner);
+        }
+        for (PropertyOwner owner : propertyOwners) {
+            owner.displaySummary();
+        }
+        sc.close();
     }
 }
