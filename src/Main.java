b0b9224976c069e0f49829c79d9c7487b94818cc
Luan327 <166567148+Luan327@users.noreply.github.com>
2024-10-13 19:33:29 -0300

    Atualiza o método main para incluir seleção de tipo de imóvel com checagem de entrada

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.print("Quantos proprietários: ");
         int quantityOwner = sc.nextInt();
 
+        String type = "";
+        int tipo = 0;
+
         for (int i = 0; i < quantityOwner; i++) {
             System.out.print("Nome proprietário: ");
             sc.nextLine();
@@ -27,25 +30,39 @@ public class Main {
             int quantityproperties = sc.nextInt();
 
             for (int j = 0; j < quantityproperties; j++) {
-                System.out.print("Tipo: (Residencial / Comercial): ");
-                sc.nextLine();
-                String type = sc.nextLine();
 
-                System.out.print("Valor : ");
+                do {
+                    System.out.println("Selecione o tipo do Imóvel:");
+                    System.out.println("1 - Residencial");
+                    System.out.println("2 - Comercial");
+                    System.out.print("Entre com o tipo : ");
+                    tipo = sc.nextInt();
+                    if (tipo == 1) {
+                        type = "Residencial";
+                    } else if (tipo == 2) {
+                        type = "Comercial";
+                    } else {
+                        System.out.println("Tipo incorreto, selecione ( 1 ou 2 )");
+                    }
+                } while (tipo != 1 && tipo != 2);
+
+                System.out.print("Valor: ");
                 double value = sc.nextDouble();
 
                 System.out.print("Anos: ");
                 int yearsOld = sc.nextInt();
+                System.out.println();
 
-
-                Property prop = new Property(type,value, yearsOld);
+                Property prop = new Property(type, value, yearsOld);
                 propertyOwner.getProperties().add(prop);
-            }
 
-            propertyOwners.add(propertyOwner);
+
+                propertyOwners.add(propertyOwner);
+            }
         }
         for (PropertyOwner propertyOwner : propertyOwners) {
             propertyOwner.displaySummary();
+            System.out.println();
         }
     }
 }
